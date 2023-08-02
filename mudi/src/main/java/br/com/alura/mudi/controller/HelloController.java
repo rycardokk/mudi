c78881ed42725c4cdf9bdcaafc9c22097dca3800
rycardokk <rycardokk@gmail.com>
2023-08-02 19:40:59 -0300

    criação do front e início do back

--- mudi/src/main/java/br/com/alura/mudi/controller/HelloController.java
+++ mudi/src/main/java/br/com/alura/mudi/controller/HelloController.java
@@ -1,8 +1,15 @@
 package br.com.alura.mudi.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloController {
 
-    public String hell(){
+    @GetMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("nome","Mundo");
         return "hello";
     }
 

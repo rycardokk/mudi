e560c79adfd55457c558c6da3189b75c1fc876b4
rycardokk <rycardokk@gmail.com>
2023-08-13 18:45:16 -0300

    Autenticacao em base de dados

---
+++ mudi/src/main/java/br/com/alura/mudi/config/secrurity/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package br.com.alura.mudi.config.secrurity;
+
+import br.com.alura.mudi.model.User;
+import br.com.alura.mudi.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userModel = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+        return userModel;
+    }
+}
---
+++ mudi/src/main/java/br/com/alura/mudi/model/User.java
@@ -0,0 +1,75 @@
+package br.com.alura.mudi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User implements UserDetails, Serializable {
+
+    private static final long serialVersionUID =1L;
+
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
